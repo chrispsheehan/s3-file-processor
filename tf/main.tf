b3c471efd68185449dabe6ab4cfafeb95ff3ec04
chrispsheehan <chrispsheehan@gmail.com>
2024-04-22 17:18:15 +0100

    reserved_concurrent_executions = 1

--- tf/main.tf
+++ tf/main.tf
@@ -29,6 +29,8 @@ resource "aws_lambda_function" "func" {
   role          = aws_iam_role.iam_for_lambda.arn
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
+
+  reserved_concurrent_executions = 1
 }
 
 resource "aws_s3_bucket" "bucket" {
