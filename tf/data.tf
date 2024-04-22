3281bc3ad0358b877a8de37b57447444e5194f0a
chrispsheehan <chrispsheehan@gmail.com>
2024-04-22 17:09:09 +0100

    lock down iam

--- tf/data.tf
+++ tf/data.tf
@@ -17,6 +17,9 @@ data "aws_iam_policy_document" "lambda_policy" {
     actions = [
       "s3:*"
     ]
-    resources = ["*"]
+    resources = [
+      aws_s3_bucket.bucket.arn, 
+      "${aws_s3_bucket.bucket.arn}/*"
+    ]
   }
 }
